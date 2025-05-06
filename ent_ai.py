a404073d5bee90bc66bedbb79fdfbe57b9513cd4
MarkBishopUcas <mark.bishop@ucas-edu.net>
2025-05-06 16:45:01 +0000

    fixed ai code T-T it took ages

--- ent_ai.py
+++ ent_ai.py
@@ -2,28 +2,46 @@ import game_assets
 import random
 
 def test_enemy_decision_tree(enemy, attack_list, status_ability_list, healing_ability_list):
-    enemies = game_assets.allies
-    allies = game_assets.enemies
+    #print(enemy)
+    #print("enemy")
+    #print("\n\n")
+    #print(attack_list)
+    #print("attack_list")
+    #print("\n\n")
+    #print(status_ability_list)
+    #print("status ability list")
+    #print("\n\n")
+    #print(healing_ability_list)
+    #print("healing ability list")
+    #print("\n\n")
 
-    decision_list = []
+    enemy_team = game_assets.enemies
+    player_team = game_assets.allies
+
+    decision_list = ["attack", "ability", "heal"]
+    #print ("created initial ability list")
 
     low_hp_enemies = []
+    #print("created empty low_hp_enemies list")
     near_death_enemies = []
+    #print("created empty near_death_enemies list")
     damaged_allies = []
+    #print("created empty damaged_allies list")
     critical_allies = []
+    #print("created empty critical_allies list")
 
-    # Evaluate enemy team
-    for i in range(len(enemies)):
-        enemy_unit = enemies[i]
+    # Evaluate own team
+    for i in range(len(enemy_team)):
+        enemy_unit = enemy_team[i]
         hp_percent = (enemy_unit.hp * 100) / enemy_unit.max_hp
         if hp_percent <= 40:
             low_hp_enemies.append(enemy_unit)
         if hp_percent <= 20:
             near_death_enemies.append(enemy_unit)
 
-    # Evaluate own team (including self)
-    for i in range(len(allies)):
-        ally = allies[i]
+    # Evaluate enemy team (including self)
+    for i in range(len(player_team)):
+        ally = player_team[i]
         hp_percent = (ally.hp * 100) / ally.max_hp
         if hp_percent <= 60:
             damaged_allies.append(ally)
@@ -38,39 +56,35 @@ def test_enemy_decision_tree(enemy, attack_list, status_ability_list, healing_ab
         critical_allies.append(enemy)
 
     if len(near_death_enemies) > 0:
-        decision_list.extend(["attack"] * 3)
+        decision_list.extend(["heal"] * 3)
     if len(damaged_allies) > 0:
-        decision_list.extend(["ability"] * 2)
-    decision_list.append("attack")
+        decision_list.extend(["heal"] * 2)
+    for i in range(3):
+        main_choice = random.choice(decision_list)
+        #print(decision_list)
+        #print(main_choice)
 
-    main_choice = random.choice(decision_list)
+        if main_choice == "attack":
+            if len(attack_list) == 0:
+                #print("no_attack_available")
+                pass
+            else:
+                return random.choice(attack_list)
 
-    if main_choice == "attack":
-        if len(attack_list) == 0:
-            return "no_attack_available"
-        return random.choice(attack_list)
-
-    if main_choice == "ability":
-        ability_type_list = []
-
-        if len(damaged_allies) >= 2:
-            ability_type_list.extend(["group_heal"] * 3)
-        if len(critical_allies) > 0:
-            ability_type_list.extend(["single_heal"] * 4)
-
-        ability_type_list.extend(["status"] * 3)
-
-        ability_type = random.choice(ability_type_list)
-
-        if "heal" in ability_type:
-            if len(healing_ability_list) == 0:
-                return "no_healing_available"
-            return random.choice(healing_ability_list)
-        else:
+        if main_choice == "ability":
             if len(status_ability_list) == 0:
-                return "no_status_ability_available"
-            return random.choice(status_ability_list)
-
+                #print("no_status_ability_available")
+                pass
+            else:
+                return random.choice(status_ability_list)
+        
+        if main_choice == "heal":
+            if len(healing_ability_list) == 0:
+                #print("no_healing_ability_available")
+                pass
+            else:
+                return random.choice(healing_ability_list)
+ 
 # --- Example Attacks and Abilities using game_assets.attack ---
 
 from game_assets import attack
@@ -94,7 +108,7 @@ shield_up = attack("shield_up", "Shield Up", "Raises a shield to reduce incoming
                    "perfect defense!", "raises a shield.", "shield slips!", "drops the shield!", 2)
 
 # Function call
-print(test_enemy_decision_tree(game_assets.enemies[0], [slash, fireball], [blind_strike, shield_up], [single_heal, group_heal]))
+#print(test_enemy_decision_tree(game_assets.enemies[0], [], [], [group_heal]))
 
 
 
