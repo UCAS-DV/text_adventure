46bec780f28dde1238f1fc7d65786f38c05262c7
UCAS-DV <darius.vaiaoga@ucas-edu.net>
2025-05-07 19:30:58 -0600

    hi

--- explore.py
+++ explore.py
@@ -2,6 +2,7 @@
 # Chicken jockey
 
 from game_assets import *
+from dialogue_reader import read_description
 
 # Avery, exploring
 
@@ -10,6 +11,7 @@ main_locations = [
    {
        "name": "Spookyland",
        "mini_locations": ["Carnival Tent", "Haunted Maze", "Graveyard", "Mirror Room", "Ghost Ship"],
+       'mini_local_desc': [['This is a carnival tent'], ['This is a haunted maze'], ['This is a graveyard'], ['This is a mirror room'], ['This is a ghost ship']],
        "npcs": ["Carnival Skeleton"],
        "item": "Monocle of Skellybones",
        "boss": "Mr. Skellybones",
@@ -85,6 +87,7 @@ def explore(location):
 
 
         print(f"\nExploring {selected}...")
+        read_description(location['mini_local_desc'][int(choice) - 1], all_allies)
 
 
         # NPC interaction (happens once per NPC)
